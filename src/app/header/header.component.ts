64059f186533372c4116d0a41f3a60fe6195c73f
Mike Huang <wellwind@mail2000.com.tw>
2021-06-27 14:58:53 +0800

    練習 *ngIf, *ngFor 和各種樣板操作應用

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  keyword = 'test';
+  isHighlight = false;
+  fontSize = 24;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  doSearch(event: MouseEvent) {
+    this.isHighlight = !this.isHighlight;
+    this.fontSize += 2;
+    this.keyword += '!';
+    console.log(event.altKey);
+    // console.log(event);
+  }
 }
